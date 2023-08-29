d1fe7b339dd32cc045ef0f7dfd0f3e8b15abdb05
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2023-08-29 14:48:31 +0100

    Added logo and Logo text; Added list item for links

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,23 @@
 import "../Home.css";
+import "../App.css";
+
 const Home = () => {
   return (
     <div className="container">
-      <div className="navbar"> 
-            <img
-              src="../../public/img/Logo.png"
-              className="logo-img"
-            />
+      <div className="navbar">
+        <div className="logo">
+          <img src="../../public/img/Logo.png" className="logo-img" />
+          <div className="logo-text">GroovyKids Store</div>
+        </div>
+
+        <div className="nav-links">
+          <ul className="links-container">
+            <li className="links-items">Home</li>
+            <li className="links-items">About</li>
+            <li className="links-items">Shop</li>
+            <li className="links-items">Contact</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
