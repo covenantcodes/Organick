8702531cab595157486ffdc6fb7e18eb1ab996e4
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2023-08-29 15:03:11 +0100

    Added Search COntainer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,11 +5,14 @@ const Home = () => {
   return (
     <div className="container">
       <div className="navbar">
+        {/* logo */}
+
         <div className="logo">
           <img src="../../public/img/Logo.png" className="logo-img" />
           <div className="logo-text">GroovyKids Store</div>
         </div>
 
+        {/* navigation links */}
         <div className="nav-links">
           <ul className="links-container">
             <li className="links-items">Home</li>
@@ -17,6 +20,13 @@ const Home = () => {
             <li className="links-items">Shop</li>
             <li className="links-items">Contact</li>
           </ul>
+        </div>
+
+        {/* Search bar */}
+        <div className="search-bar">
+          <div className="search-box">
+              <input type="text" placeholder="Search" className="search-input" />
+          </div>
         </div>
       </div>
     </div>
